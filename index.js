29d437679052b331da82d7edb00a20dfc7e948e3
Giovanne <giovanneafonso@gmail.com>
2018-05-30 19:03:42 -0300

    Improve code quality + Add comments

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require('http');
 
+// Optional env variables
 const name = process.env.NAME || 'kubernetes-env-debugger';
 const port = process.env.PORT || '80';
 
-const app = new http.Server();
-
-const getEnvHtml = function() {
+// Generate env html
+function getEnvHtml() {
   let html = '<table><thead><tr><th>ENV NAME</th><th>ENV VALUE</th></tr><tr><td colspan="2"></td></tr></thead><tbody>';
   for(let key in process.env) {
     if(process.env.hasOwnProperty(key)) {
@@ -16,12 +16,13 @@ const getEnvHtml = function() {
   return html;
 };
 
-app.on('request', (req, res) => {
+// Create the server
+const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write(`<!doctype html><html><body>${getEnvHtml()}</body></html>`);
   res.end('\n');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`${name} is listening on port ${port}`);
 });
